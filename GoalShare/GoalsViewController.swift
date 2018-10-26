3e33c2f079ff461cbe4cc1d6f330603e343f50e1
User <user@users-mbp-3>
2018-10-26 11:20:10 -0400

    Added a new view controller that allows you to create a new goal. Currently broken due to a firebase configure issue

--- GoalShare/GoalsViewController.swift
+++ GoalShare/GoalsViewController.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class GoalsViewController: UIViewController {
-
+    
+    @IBOutlet weak var goalNameTxt: UITextField!
+    @IBOutlet weak var goalDescriptionTxt: UITextField!
+    
+    let dbref = Database.database().reference()
+    
+    
+    @IBAction func DoneButtonPressed(_ sender: Any) {
+        //This is currently hardcoded for test purposes. Will change
+        let gid = "0"
+        if let goalDes = goalDescriptionTxt.text {
+            if goalDes != "" {
+                let uid = Auth.auth().currentUser?.uid
+                self.dbref.child("goals").child(gid).setValue(["gid":1, "goal_description":goalDes])
+            self.dbref.child("goals").child(gid).child("uids").child(uid!).setValue(["completed":"false"])
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
