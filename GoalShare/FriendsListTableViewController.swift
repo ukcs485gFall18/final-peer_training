71e4792ffff8adc59ee2585c5df1f63ff91d128a
Bryan Willis <bryanwillis200@gmail.com>
2018-11-05 11:51:41 -0500

    Finished the add friend functionality. Added the functionality to view all current friends.

--- GoalShare/FriendsListTableViewController.swift
+++ GoalShare/FriendsListTableViewController.swift
@@ -11,8 +11,7 @@ import Firebase
 import FirebaseDatabase
 
 class FriendsListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet var tableView: UITableView!
-    
+    @IBOutlet var friendTableView: UITableView!
     var ref:DatabaseReference?
     var databaseHandle:DatabaseHandle?
     var postData = [String]()
@@ -27,30 +26,30 @@ class FriendsListTableViewController: UIViewController, UITableViewDelegate, UIT
         let currentUserId = Auth.auth().currentUser?.uid
         
         // Fill cell from postData
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = postData[indexPath.row]
-        return cell!
+        let thisCell = friendTableView.dequeueReusableCell(withIdentifier: "friendCell")
+        thisCell?.textLabel?.text = postData[indexPath.row]
+        print(postData[indexPath.row])
+        return thisCell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        /*//Set current user ID
-        let currentUserId = Auth.auth().currentUser?.uid
+        //Set current user ID
+        //let currentUserId = Auth.auth().currentUser?.uid
         
         // Get text for current row
-        let currentCell = tableView.cellForRow(at: indexPath) as! UITableViewCell
-        let cellName = currentCell.textLabel!.text
+        let currentCell = friendTableView.cellForRow(at: indexPath) as! UITableViewCell
+        //let cellName = currentCell.textLabel!.text
         
         // Set value to true
-        ref!.child("goals").child(cellName!).child("uids").child(currentUserId!).setValue(["completed":"true"])*/
+        //ref!.child("goals").child(cellName!).child("uids").child(currentUserId!).setValue(["completed":"true"])
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        /*
-        tableView.delegate = self
-        tableView.dataSource = self
+        friendTableView.delegate = self
+        friendTableView.dataSource = self
         
         //Set the firebase reference
         ref = Database.database().reference()
@@ -59,25 +58,17 @@ class FriendsListTableViewController: UIViewController, UITableViewDelegate, UIT
         let currentUserId = Auth.auth().currentUser?.uid
         
         //Retrieve posts and listen for changes
-        databaseHandle = ref?.child("goals").observe(.childAdded, with: { (snapshot) in
-            
-            //Code that is executed when a new goal is added
-            // Add value from snapshot to postData if uid matches current user
-            let goalName = snapshot.key
-            for goal in snapshot.children {
-                let goalSnap = goal as! DataSnapshot
-                if (goalSnap.key == "uids"){
-                    for uid in goalSnap.children {
-                        let uidSnap = uid as! DataSnapshot
-                        if (uidSnap.key == currentUserId){
-                            self.postData.append(goalName)
-                        }
-                    }
-                }
-            }
-            self.tableView.reloadData()
+        databaseHandle = ref?.child("friends").child(currentUserId!).observe(.childAdded, with: { (snapshot) in
+            for users in snapshot.children {
+                print("snap key = \(snapshot.key)")
+                let friendSnap = users as! DataSnapshot
+                print("friendSnap key = \(friendSnap.key)")
+                self.postData.append(friendSnap.value as! String)
+             }
+            self.friendTableView.reloadData()
             //self.postData.append()
         })
-        */
     }
 }
+
+
--- GoalShare/FriendsViewController.swift
+++ GoalShare/FriendsViewController.swift
@@ -12,5 +12,36 @@ import FirebaseDatabase
 
 class FriendsViewController: UIViewController {
   
-    @IBOutlet weak var friendNameTxt: UITextField!
+
+    @IBOutlet weak var NameTxt: UITextField!
+    var dbref : DatabaseReference!
+    var anyValue: AnyObject?
+    let uid = Auth.auth().currentUser?.uid
+    
+    @IBAction func Add_Button_Pressed(_ sender: Any) {
+        if let Name = NameTxt.text {
+            if (Name != "") {
+                //search the database for the user
+                dbref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+                    for snap in snapshot.children {
+                        let userSnap = snap as! DataSnapshot
+                        let friendUid = userSnap.key //the uid of each user
+                        let userDict = userSnap.value as! [String:AnyObject] //child data
+                        let userName = userDict["uname"] as! String
+                        if (Name == userName) {
+                            self.dbref.child("friends").child(self.uid!).child("uids").child(friendUid).setValue(Name)
+                                print("Friend Added!")
+                        }
+                    }
+                })
+            }
+        }
+    } //Add_Button_pressed */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        dbref =  Database.database().reference()
+        // Do any additional setup after loading the view.
+    }
 }
