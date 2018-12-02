c7b791956964cbaab98a57ccbb4047d88e98a312
Conner Hurst <hurst3370@gmail.com>
2018-12-01 19:53:46 -0500

    The app now logs completed goals. Still need to add some checks to prevent it from making multiple logs for the same goal in one day, etc.

--- GoalShare/GoalListViewController.swift
+++ GoalShare/GoalListViewController.swift
@@ -48,19 +48,6 @@ class GoalListViewController: UIViewController, UITableViewDelegate, UITableView
         return cell!
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-//    {
-//        //Set current user ID
-//        let currentUserId = Auth.auth().currentUser?.uid
-//
-//        // Get text for current row
-//        let currentCell = tableView.cellForRow(at: indexPath) as! UITableViewCell
-//        let cellName = currentCell.textLabel!.text
-//
-//        // Set value to true
-//        ref!.child("goals").child(cellName!).child("uids").child(currentUserId!).setValue(["completed":"true"])
-//    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -121,36 +108,37 @@ class GoalListViewController: UIViewController, UITableViewDelegate, UITableView
             // Set value to true
             self.ref!.child("goals").child(cellName!).child("uids").child(currentUserId!).setValue(["completed":"true"])
             
-//            // Store in log
-//            // increment logid
-//            var logId = 0
+            // Store in log
+            //get current date
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM.dd.yyyy"
+            let currentDate = formatter.string(from: date)
             
-//            //get current date
-//            let date = Date()
-//            let formatter = DateFormatter()
-//            formatter.dateFormat = "MM.dd.yyyy"
-//            let currentDate = formatter.string(from: date)
-            
-//            // get goalid
-//            var goalId = 0
-//            self.databaseHandle = self.ref?.child("goals").child(cellName!).child("gid").observe(.value, with: { (DataSnapshot) in
-//                goalId = (DataSnapshot.value as? Int)!
-//            })
-            
-//            self.ref?.child("goalHistory").child(currentUserId!).observe(.value, with: { (snapshot) in
-//                // increment logid
-//                for id in snapshot.children {
-//                    let idSnap = id as! DataSnapshot
-//                    let idInt = Int(idSnap.key)
-//                    if (idInt! > logId){
-//                        logId = idInt!
-//                    }
-//                }
-//                logId += 1
-//                self.postLogId = String(logId)
-//            })
-//            var logIdQuery = self.ref!.child("goalHistory").child(currentUserId!).queryOrderedByKey().queryLimited(toLast: 1)
-            
+            // get goalid
+            var goalId = 0
+            self.databaseHandle = self.ref?.child("goals").child(cellName!).child("gid").observe(.value, with: { (DataSnapshot) in
+                goalId = (DataSnapshot.value as? Int)!
+            })
+
+            // get incremented logid and log data
+            var logIdQuery = self.ref!.child("goalHistory").child(currentUserId!).queryOrderedByKey().queryLimited(toLast: 1)
+            logIdQuery.observeSingleEvent(of: .value, with: {
+                snapshot in
+                guard let fetchedData = snapshot.children.allObjects as? [DataSnapshot] else {return}
+                var key = ""
+                for item in fetchedData {
+                    key = item.key
+                }
+                if let maxLogId = Int(key) {
+                    self.postLogId = String(maxLogId + 1)
+                } else {
+                    self.postLogId = String(0)
+                }
+                
+                // log completed goal
+                self.ref?.child("goalHistory").child(currentUserId!).child(self.postLogId).setValue(["completeDate": currentDate, "gid": goalId])
+            })
         }
         complete.backgroundColor = .lightGray
         
