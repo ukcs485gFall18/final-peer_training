80ece87228feabfd72903ceed52c0b0d8b1b3a1d
Seanna Lea LoBue <qarylla@gmail.com>
2018-12-03 23:37:35 -0500

    Additional image for Message Button. Initial set up for custom data cells for summary view.

--- GoalShare/MainViewController.swift
+++ GoalShare/MainViewController.swift
@@ -26,15 +26,10 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                     "My Goals"]
     var items = [[FriendModel]() as [Any],
                  [GoalModel]()]
-
+    var tintColors = [ #colorLiteral(red: 0.3182998379, green: 0.3019897466, blue: 0.4855987017, alpha: 0.9021476506), #colorLiteral(red: 0.21545305, green: 0.4786607049, blue: 0.5639418172, alpha: 0.9021476506), #colorLiteral(red: 0.3869009155, green: 0.696799651, blue: 0.3262433093, alpha: 0.9021476506), #colorLiteral(red: 0.696799651, green: 0.04511301659, blue: 0.0009451018385, alpha: 0.9021476506), #colorLiteral(red: 0.7189426104, green: 0.71675867, blue: 0.27614689, alpha: 0.9021476506) ]
     
     // View Display and Data Functions
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        self.summaryTableView.reloadData()
-    }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -59,15 +54,15 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                             goals.append(fgoal)
                         }
                         friend.goals = goals
-                        //print("*** Count of friend goals = \(friend.goals.count)")
+                        print("*** Count of friend goals = \(friend.goals.count)")
                     })
                     if(!self.friendData.contains(friend)){
                         friend.completePercent = friend.calculateCompletionRate()
                         self.friendData.append(friend)
                     }
-                    self.items[0] = self.friendData
-                    self.summaryTableView.reloadData()
                 }
+                self.items[0] = self.friendData
+                self.summaryTableView.reloadData()
             })
         }
         
@@ -87,7 +82,6 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidAppear(_ animated: Bool) {
         self.summaryTableView.reloadData()
     }
-    
     
     // Table - Delegate Functions
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -114,6 +108,12 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                 friend.completePercent = friend.calculateCompletionRate()
                 print("*** \(friend.nickName) has completed \(friend.completePercent)% of their goals.")
                 cell?.detailTextLabel?.text = "\(friend.nickName) has completed \(friend.completePercent)% of their goals."
+                let tindex = friend.completePercent == 0 ? 0 : Int(friend.completePercent / 20) - 1
+                let timage = UIImage(named: "message")?.withRenderingMode(.alwaysTemplate)
+                let msgBtn = UIButton()
+                msgBtn.setImage(timage, for: .normal)
+                msgBtn.tintColor = tintColors[tindex]
+                cell?.addSubview(msgBtn)
             }else{
                 cell?.textLabel?.text = "No Friends Found"
                 cell?.detailTextLabel?.text = "Go to the Friends menu to add a friend."
