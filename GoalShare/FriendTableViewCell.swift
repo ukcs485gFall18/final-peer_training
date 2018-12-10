111e46459f339ad799bcf7a61b6879caa069ba69
Seanna Lea LoBue <qarylla@gmail.com>
2018-12-10 17:13:06 -0500

    Created Message Alert that updates database with user message.

--- GoalShare/FriendTableViewCell.swift
+++ GoalShare/FriendTableViewCell.swift
@@ -19,64 +19,6 @@ class FriendTableViewCell: UITableViewCell, UITextFieldDelegate {
     var dbref : DatabaseReference!
     var userName = ""
     var fname = ""
-    // create alert to write notification to db
-    @IBAction func msgFriend(_ sender: UIButton){
-        // get information for logged in user
-        guard let currentUser = Auth.auth().currentUser?.uid else{
-            print("No authenticated user found. Please relogin.")
-            return
-        }
-        // build message sender information from button
-        guard let fuid = sender.currentTitle else{
-            print("Friend ID not found.")
-            return
-        }
-
-        let userHandle = dbref?.child("users").child(currentUser).observe(.value, with: { (snapshot) in
-            for snap in snapshot.children{
-                let userSnap = snap as! DataSnapshot
-                let userDict = userSnap.value as! [String: Any]
-                self.userName = userDict["uname"] as! String
-                let msgHandle = self.dbref?.child("users").child(sender.currentTitle!).observe(.value, with: { (snapshot) in
-                    for snap in snapshot.children{
-                        let friendSnap = snap as! DataSnapshot
-                        let friendDict = friendSnap.value as! [String : Any]
-                        self.fname = friendDict["uname"] as! String
-                        print("*** Friend Name \(self.fname) - \(fuid)")
-                        print("*** Variable check: FName = \(self.fname), Username = \(self.userName)")
-                    }
-                })
-            }
-        })
-        
-        // set message date
-        let date = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyyMMddHHmmss"
-        let currentDate = formatter.string(from: date)
-
-        
-        
-        let actionTitle = "Message to \(fname)"
-        // create controller with additional text field
-        let message = UIAlertController(title: "", message: "Send Message to \(userName)", preferredStyle: UIAlertController.Style.alert)
-        message.addTextField(configurationHandler: { (textField) in
-                textField.placeholder = "Message to \(self.fname)"
-                textField.text = "Keep up the good work!"
-        })
-        let messageAction: UIAlertAction = UIAlertAction(title: actionTitle, style: UIAlertAction.Style.default)
-        { (alertAction) -> Void in
-            let message = message.textFields![0].text!
-            
-            self.dbref.child("notifications").child(currentUser).child(fuid).child(currentDate).setValue(["nid": currentDate, "fname": self.fname, "message": message, "isRead":false])
-        }
-        message.addAction(messageAction)
-        
-        /*OperationQueue.main.addOperation{ () -> Void in
-            self.present(message, animated: true, completion: nil)
-        }
- */
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
